a3a25d5bc6f8346b7e802087776d737116f58a5d
nguyenan312 <goals936@gmail.com>
2022-07-18 16:37:36 +0700

    - Get metadata and parse json

--- Assets/SCRIPTS/API Calling/API_Endpoints.cs
+++ Assets/SCRIPTS/API Calling/API_Endpoints.cs
@@ -10,6 +10,7 @@ namespace NewLifeZ.API
 
         public const string LOGIN = "https://dev.api.newlifez.io/v1.0/api/auth/sign-in";
         public const string SIGNUP = BASE_URL + "/auth/signup";
+        public const string GET_JSON_METADATA = "https://dev.api.newlifez.io/v1.0/api/user/token";
     }
 
 }
