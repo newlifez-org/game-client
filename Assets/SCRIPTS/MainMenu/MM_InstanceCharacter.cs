83ae637bf94f1e063867ff755101d42fef442e65
thanhUni0708 <thanhtm78@akagames.io>
2022-07-19 15:11:50 +0700

    Add Show Character

--- Assets/SCRIPTS/MainMenu/MM_InstanceCharacter.cs
+++ Assets/SCRIPTS/MainMenu/MM_InstanceCharacter.cs
@@ -7,7 +7,20 @@ namespace NewLifeZ.MainMenu
 {
     public class MM_InstanceCharacter : MonoBehaviour
     {
-        
+        public GameObject ModelPrefab;
+
+        private void Start()
+        {
+            InstanceCharacter();
+        }
+
+        void InstanceCharacter()
+        {
+            for(int i = 0; i < API_Static.m_CharacterMetaData.Count; i++)
+            {
+                Instantiate(ModelPrefab, transform);
+            }
+        }
     }
 }
 
